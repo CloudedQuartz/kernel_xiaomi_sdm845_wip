1c0f2330d357327991091332fc12ddd75c0891c0
Zachariah Kennedy <zkennedy87@gmail.com>
2019-06-20 05:15:54 +0000

    sched/fair: Optimize Schedtune boosted task placement
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: Kailash <kailash.sudhakar@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6521,7 +6521,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 		return true;
 
 	if (task_boost_policy(p) == SCHED_BOOST_ON_BIG &&
-			is_min_capacity_cpu(cpu))
+			is_min_capacity_cpu(cpu) || schedtune_task_boost(p) > 0)
 		return false;
 
 	return __task_fits(p, cpu, 0);
