f5c794308646e2f998f6d739f4df9630d4886308
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-10 10:51:48 +0530

    kernel: Use task struct instead of pid to check for zygote
    Pids can be reused and thus are not a stable way of checking if a task
    is zygote. Check the task_struct pointer instead.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2102,7 +2102,7 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid))
+	if (task_is_zygote(current))
 		cpu_input_boost_kick_max(50);
 
 	/*
