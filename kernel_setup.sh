b4ec5204f7f40707d952e367ba838ad1d06030c3
CloudedQuartz <ravenklawasd@gmail.com>
2021-02-26 20:57:55 +0530

    kernel: Init drone ci
    Change-Id: I6971558fa0f39eaf43f1fda731354c1b3b7ec6b5
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

---
+++ kernel_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="SilverCore"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/painkiller3/AnyKernel3"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 $TC_DIR/arm64
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm  $TC_DIR/arm
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh
