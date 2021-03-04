a1d58cd7727c3dfe8da6e13415c00443e505dd49
CloudedQuartz <ravenklawasd@gmail.com>
2021-03-04 17:43:50 +0530

    drm: msm: sde: Also boost LLCCBw when a frame is ready
    Following b36be6689eba483a6b3fd6e628dfd8c8b0522052
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -3976,6 +3976,7 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc,
 	SDE_ATRACE_BEGIN("crtc_commit");
 
 	devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+ 	devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
 
 	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
 
