MotorolaMobilityLLC/kernel-msm

clk: qcom: clk-cpu-osm: Notify the current frequency to the topology driver
The topology driver keeps track of the current capacity of a CPU and
make it available via arch_scale_freq_capacity(). The PELT load
tracking in the scheduler and energy-aware task placement are the
clients of this API. Hence notify the new frequency to the topology
driver during frequency transition.

Change-Id: Ia0890956f59082496e7d5d7ba8a9c09b343e44ec
Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

drivers/clk/qcom/clk-cpu-osm.c
@@ -496,6 +496,9 @@ osm_cpufreq_target_index(struct cpufreq_policy *policy, unsigned int index)
 	struct clk_osm *c = policy->driver_data;
 
 	osm_set_index(c, index, c->core_num);
+	arch_set_freq_scale(policy->related_cpus,
+			    policy->freq_table[index].frequency,
+			    policy->cpuinfo.max_freq);
 	return 0;
 }
 

CloudedQuartz/kernel_xiaomi_sdm845_wip

Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

drivers/clk/qcom/clk-cpu-osm.c
@@ -709,6 +709,9 @@ osm_cpufreq_target_index(struct cpufreq_policy *policy, unsigned int index)
 	struct clk_osm *c = policy->driver_data;
 
 	osm_set_index(c, index);
+	scale_freq_capacity(policy->related_cpus,
+			    policy->freq_table[index].frequency,
+			    policy->cpuinfo.max_freq);
 	return 0;
 }
 